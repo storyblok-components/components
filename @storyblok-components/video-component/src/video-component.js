079e21162766ed74c25e2f051a2fba7f3b8cacdb
Marcin Krawczyk <marckraw@icloud.com>
2020-05-11 00:38:42 +0200

    fix(video-component): add storyblok editable to component

--- @storyblok-components/video-component/src/video-component.js
+++ @storyblok-components/video-component/src/video-component.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import ReactPlayer from 'react-player';
+import SbEditable from 'storyblok-react';
 import './video-component.scss';
 
 const VideoComponent = ({ blok }) => {
   const autoplay = !!blok.autoplay;
   return (
-    <div className="player-wrapper">
-      <ReactPlayer
-        url={blok.url || ''}
-        className="react-player"
-        width="100%"
-        height="100%"
-        playing={autoplay}
-        muted={autoplay}
-        playsinline
-      />
-    </div>
+    <SbEditable content={blok}>
+      <div className="player-wrapper">
+        <ReactPlayer
+          url={blok.url || ''}
+          className="react-player"
+          width="100%"
+          height="100%"
+          playing={autoplay}
+          muted={autoplay}
+          playsinline
+        />
+      </div>
+    </SbEditable>
   );
 };
 
