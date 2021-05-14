3b520443c56cc21fcfa2721de8b65b04d965d278
Marcin Krawczyk <marckraw@icloud.com>
2021-05-14 19:14:29 +0200

    fix(build and deploy): for some reason cusomt __document.js has to be a class ? who knows

--- @docs/pages/_document.js
+++ @docs/pages/_document.js
@@ -1,17 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-const CustomDocument = () => {
-  return (
-    <Html>
-      <Head>
-        <title>@storyblok-components documentation</title>
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
-};
+class CustomDocument extends Document {
+  // eslint-disable-next-line class-methods-use-this
+  render() {
+    return (
+      <Html>
+        <Head>
+          <title>@storyblok-components documentation</title>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
 
 export default CustomDocument;
