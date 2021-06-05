a8ff72b12b0edb7f4120c4dac9f01cf535f6d22f
Marcin Krawczyk <marckraw@icloud.com>
2021-06-05 20:30:30 +0200

    fix(twnd-card): remove not needed console.logs from card

--- @storyblok-components/twnd-card/src/twnd-card.js
+++ @storyblok-components/twnd-card/src/twnd-card.js
@@ -3,19 +3,17 @@ import SbEditable from 'storyblok-react';
 // import cn from 'classnames';
 
 const TwndCard = (props) => {
-  console.log('these are props from twnd card');
-  console.log(props);
-
   const { blok } = props;
   const { content, heading, image } = blok;
-
-  console.log('and this is image prop');
   console.log(image);
 
   return (
     <SbEditable content={blok}>
       <div>
-        <img src="https://source.unsplash.com/random" alt="randopm image" />
+        <img
+          src="https://source.unsplash.com/user/erondu/160x120"
+          alt="randopm image"
+        />
         <h2>{heading}</h2>
         <p>{content}</p>
       </div>
